fb88b47c708f6c72729a221852b79d509514d9fd
micahkepe <micahkepe@gmail.com>
2025-11-13 20:07:08 -0600

    feat(builder): pass tests 01, 02

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,8 +1,32 @@
 use proc_macro::TokenStream;
+use proc_macro2::Ident;
+use quote::quote;
+use syn::{DeriveInput, parse_macro_input};
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = input;
+    let ast = parse_macro_input!(input as DeriveInput);
+    let name = &ast.ident;
+    let builder_name = format!("{}Builder", name);
+    let builder_ident = Ident::new(&builder_name, name.span());
+    let expanded = quote! {
+        pub struct #builder_ident {
+            executable: Option<String>,
+            args: Option<Vec<String>>,
+            env: Option<Vec<String>>,
+            current_dir: Option<String>,
+        }
 
-    unimplemented!()
+        impl #name {
+            fn builder() -> #builder_ident {
+                #builder_ident {
+                    executable: None,
+                    args: None,
+                    env: None,
+                    current_dir: None,
+                }
+            }
+        }
+    };
+    TokenStream::from(expanded)
 }
